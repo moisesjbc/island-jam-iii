3289f80c1a3962e698b8797469b57f8c2a870c95
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-21 15:52:23 +0100

    Added quick time events

---
+++ Island Jam 3/Assets/scripts/FlashingAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingAction : MonoBehaviour {
+	public float flashingTime = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Activate () {
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+
+	public void Deactivate () {
+		GetComponent<SpriteRenderer>().color = Color.white;
+	}
+}
---
+++ Island Jam 3/Assets/scripts/QuickTimeEvent.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTimeEvent : MonoBehaviour {
+	public float timeout = 3.0f;
+	public float timeoutBetweenEvents = 1.0f;
+
+	public FlashingAction[] actions;
+	public KeyCode[] keys;
+
+	private FlashingAction currentAction = null;
+	private KeyCode currentKey;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("actions");
+		Debug.Log (actions.Length);
+		StartQuickTimeEvents ();
+	}
+
+	void Update () {
+		if (currentAction != null) {
+			if (Input.GetKeyDown (currentKey)) {
+				Debug.Log ("Good!");
+				RestartQuickTimeEvents ();
+			} else if (Input.anyKeyDown) {
+				Debug.Log ("Bad!");
+				RestartQuickTimeEvents ();
+			}
+		}
+	}
+
+
+	void StartQuickTimeEvents() {
+		StartCoroutine ("QuickimeEventsCoroutine");
+	}
+
+	void StopQuickTimeEvents() {
+		StopCoroutine ("QuickimeEventsCoroutine");
+	}
+
+	void RestartQuickTimeEvents() {
+		StopQuickTimeEvents ();
+		if (currentAction != null) {
+			currentAction.Deactivate ();
+		}
+		StartQuickTimeEvents ();
+	}
+
+	IEnumerator QuickimeEventsCoroutine() {
+		while (true) {
+			yield return new WaitForSeconds (timeoutBetweenEvents);
+
+			int randomIndex = Random.Range (0, 3);
+			currentAction = actions [randomIndex];
+			currentKey = keys [randomIndex];
+			currentAction.Activate ();
+			yield return new WaitForSeconds (timeout);
+			if (currentAction != null) {
+				currentAction.Deactivate ();
+				currentAction = null;
+			}
+		}
+	}
+}
