8da2eab67566996d732250a56cc49e20de49f7bd
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-21 23:47:36 +0100

    Fixed no defined error after deleting game over panel

--- Island Jam 3/Assets/scripts/GameController.cs
+++ Island Jam 3/Assets/scripts/GameController.cs
@@ -3,20 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
-
-	public GameObject gameOverPanel = null;
-
-	// Use this for initialization
-	void Start () {
-		gameOverPanel.SetActive (false);
-	}
-		
+public class GameController : MonoBehaviour {	
 	public void GameOver(){
 		SceneManager.LoadScene("game_over_scene");
-	}
-
-	public void Restart(){
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
