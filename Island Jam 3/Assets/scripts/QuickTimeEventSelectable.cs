f0153129a7ae34858098b32b520ab83a201ec92f
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-21 17:41:47 +0100

    Added prefab + selection

--- Island Jam 3/Assets/scripts/QuickTimeEventSelectable.cs
+++ Island Jam 3/Assets/scripts/QuickTimeEventSelectable.cs
@@ -6,19 +6,34 @@ public class QuickTimeEventSelectable : MonoBehaviour {
 
 	public Rompible rompible;
 	public QuickTimeEvent quickTimeEvent;
+	private bool selected = false;
+	public Camera camera;
 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (selected && Input.GetMouseButtonDown(0)) {
+			Vector3 mousePosition = this.camera.ScreenToWorldPoint (Input.mousePosition);
+			Bounds bounds = GetComponent<SpriteRenderer> ().bounds;
+			mousePosition.z = bounds.center.z;
+
+			Debug.Log (mousePosition);
+			Debug.Log (bounds);
+
+			if (!bounds.Contains (mousePosition)) {
+				selected = false;
+				GetComponent<SpriteRenderer> ().color = Color.white;
+			}
+		}
 	}
 
 	void OnMouseDown() {
+		selected = true;
 		GetComponent<SpriteRenderer> ().color = Color.cyan;
 		quickTimeEvent.Activate (rompible);
 	}
