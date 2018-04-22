753a4ce0d3a5836377e94813f80dbb46e3960786
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-22 16:04:28 +0100

    Added how to play

---
+++ Island Jam 3/Assets/TutorialBook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialBook : MonoBehaviour {
+
+	public Sprite [] backgrounds;
+
+	[TextArea(3,10)]
+	public string[] texts;
+	int currentPage = 0;
+	public Text textBox;
+	
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image>().sprite = backgrounds[0];
+		textBox.text = texts [0];
+	}
+
+	public void NextPage () {
+		currentPage = currentPage + 1;
+		if (currentPage > backgrounds.Length) {
+			currentPage++;
+		}
+		GetComponent<Image>().sprite = backgrounds[currentPage];
+		textBox.text = texts [currentPage];
+	}
+
+	public void PreviousPage () {
+		currentPage = (currentPage - 1);
+		if (currentPage < 0) {
+			currentPage = 0;
+		}
+		GetComponent<Image>().sprite = backgrounds[currentPage];
+		textBox.text = texts [currentPage];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
