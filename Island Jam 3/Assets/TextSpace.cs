d13602f48c5fb29a84f9d75739e58e3da5f53d48
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-22 22:00:21 +0100

    Added translations

---
+++ Island Jam 3/Assets/TextSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSpace : MonoBehaviour {
+
+	[TextArea(3, 10)]
+	public string[] texts;
+	int currentIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			currentIndex++;
+			if (currentIndex < texts.Length) {
+				GetComponent<Text> ().text = texts [currentIndex];
+			}
+		}
+	}
+}
