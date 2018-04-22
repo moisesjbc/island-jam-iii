5db22c8f28bd3ee4f90eedb327d7cfc07b0d341e
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-22 15:19:15 +0100

    Added runes

--- Island Jam 3/Assets/scripts/FlashingAction.cs
+++ Island Jam 3/Assets/scripts/FlashingAction.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class FlashingAction : MonoBehaviour {
 	public float flashingTime = 0.1f;
 
+	public Sprite inactiveSprite;
+	public Sprite activeSprite;
+	public Sprite pressedSprite;
+	public KeyCode key;
+
+	float timeout = -1.0f;
+
+	bool active = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,14 +21,16 @@ public class FlashingAction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	public void Activate () {
-		GetComponent<SpriteRenderer>().color = Color.red;
+		active = true;
+		GetComponent<SpriteRenderer> ().sprite = activeSprite;
+
 	}
 
 	public void Deactivate () {
-		GetComponent<SpriteRenderer>().color = Color.white;
+		active = false;
+		GetComponent<SpriteRenderer> ().sprite = inactiveSprite;
 	}
 }
