28204d4d07152b51d1122a13c8b02000cacd361f
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-22 10:29:07 +0100

    Added timer

---
+++ Island Jam 3/Assets/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	float time = 0.0f;
+
+	void Update () {
+		time += Time.deltaTime;
+		int seconds = (int)time % 60;
+		int minutes = (int)time / 60;
+		GetComponent <Text> ().text = minutes.ToString ("#00") + ":" + seconds.ToString ("#00");
+	}
+}
