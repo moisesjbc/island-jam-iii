9ee109ead9d4e2015add8ce4bd1ee9ab1d7e6959
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-22 15:26:00 +0100

    Fixed angry bar not going to game over when full

--- Island Jam 3/Assets/scripts/DamageBar.cs
+++ Island Jam 3/Assets/scripts/DamageBar.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageBar : MonoBehaviour {
+	public GameController gameController;
+
 	public void SetValue (float value) {
 		if (value > 100.0f) {
+			gameController.GameOver ();
 			value = 100.0f;
 		} else if (value < 0.0f) {
 			value = 0.0f;
