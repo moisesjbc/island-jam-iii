44f22453ad3d4367a9f70ad0d1ed7d6c85634f8d
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-04-23 22:35:48 +0100

    Fixed navigation in how to play screen

--- Island Jam 3/Assets/scripts/TutorialBook.cs
+++ Island Jam 3/Assets/scripts/TutorialBook.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class TutorialBook : MonoBehaviour {
 
 	public Sprite [] backgrounds;
+	public Button previousPageButton;
+	public Button nextPageButton;
 
 	[TextArea(3,10)]
 	public string[] texts;
@@ -19,20 +21,27 @@ public class TutorialBook : MonoBehaviour {
 		textBox.text = texts [0];
 	}
 
+	public void UpdateButtons (int currentPage) {
+		nextPageButton.gameObject.SetActive(currentPage < (backgrounds.Length - 1));
+		previousPageButton.gameObject.SetActive (currentPage > 0); 
+	}
+
 	public void NextPage () {
 		currentPage = currentPage + 1;
-		if (currentPage > backgrounds.Length) {
-			currentPage++;
+		if (currentPage >= backgrounds.Length - 1) {
+			currentPage = backgrounds.Length - 1;
 		}
+		UpdateButtons (currentPage);
 		GetComponent<Image>().sprite = backgrounds[currentPage];
 		textBox.text = texts [currentPage];
 	}
 
 	public void PreviousPage () {
 		currentPage = (currentPage - 1);
-		if (currentPage < 0) {
+		if (currentPage <= 0) {
 			currentPage = 0;
 		}
+		UpdateButtons (currentPage);
 		GetComponent<Image>().sprite = backgrounds[currentPage];
 		textBox.text = texts [currentPage];
 	}
